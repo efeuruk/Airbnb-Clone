44d66ef3fb91c9ca4e7bd843a094b846f15e7071
Efe Uruk <e.uruk@emakina.com.tr>
2019-11-26 21:46:52 +0300

    Slick is fully implemented on homes page

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,19 +10,48 @@ function toggleNavbar() {
 }
 
 $(document).ready(function() {
-    // $('.slick-container').slick({
-    //     dots: true,
-    //     prevArrow: $('.slick-prev'),
-    //     nextArrow: $('.slick-next'),
-        
-    // });
-
+    // Slick
+    // Homes
+    $('.slick-container-card-travel').slick({
+        infinite: false,
+        slidesToShow: 2,
+        draggable: true,
+        mobileFirst: true,
+        prevArrow: $('.slick-prev-card'),
+        nextArrow: $('.slick-next-card'),
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 1119,
+                settings: {
+                    slidesToShow: 4
+                }
+            },
+            {
+                breakpoint: 1439,
+                settings: {
+                    slidesToShow: 4
+                }
+            }
+        ],
+    });
+    
     $('.slick-container').each(function(){
+        $('.slick-slide').bind('touchstart', function(){ console.log('touchstart') })
         var slickInduvidual = $(this);
         slickInduvidual.slick({
             dots: true,
+            mobileFirst: true,
+            arrows: true,
+            swipeToSlide: true,
+            touchMove: true,
             prevArrow: slickInduvidual.siblings('.slick-prev'),
-            nextArrow: slickInduvidual.siblings('.slick-next')
+            nextArrow: slickInduvidual.siblings('.slick-next'),
         });
     });
 });
