5b51ab38ee12a11b594978494e92974abbb75ffa
efeuruk <e.uruk@emakina.com.tr>
2019-12-05 23:12:26 +0300

    Webpack is configured  and its problems are resolved

---
+++ src/assets/js/index.js
@@ -0,0 +1,35 @@
+// Lodash
+import _ from 'lodash';
+
+// Bootstrap
+import 'bootstrap/js/dist/alert'
+import 'bootstrap/js/dist/button'
+// import 'bootstrap/js/dist/carousel'
+import 'bootstrap/js/dist/collapse'
+import 'bootstrap/js/dist/dropdown'
+import 'bootstrap/js/dist/modal'
+import 'bootstrap/js/dist/popover'
+import 'bootstrap/js/dist/scrollspy'
+import 'bootstrap/js/dist/tab'
+// import 'bootstrap/js/dist/toast'
+// import 'bootstrap/js/dist/tooltip'
+import 'bootstrap/js/dist/util'
+
+// Slick
+import 'slick-carousel';
+
+// Dotdotdot
+import 'dotdotdot-js';
+
+// Font Awesome
+// import '@fortawesome/fontawesome-free/js/fontawesome'
+// import '@fortawesome/fontawesome-free/js/solid'
+// import '@fortawesome/fontawesome-free/js/regular'
+// import '@fortawesome/fontawesome-free/js/brands'
+
+// My JS
+import './custom.js';
+import './rooms.js';
+
+// SCSS
+import '../styles/scss/main.scss';
---
+++ webpack.config.js
@@ -0,0 +1,106 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+// const GoogleFontsPlugin = require("@beyonk/google-fonts-webpack-plugin");
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/assets/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'production',
+  devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      port: 7000,
+      watchContentBase: true,
+      open: true
+  },
+  plugins: [
+    // new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html'
+    }),
+    new HtmlWebpackPlugin({
+        template: './src/homes.html',
+        filename: 'homes.html'
+    }),
+    new HtmlWebpackPlugin({
+        template: './src/rooms.html',
+        filename: 'rooms.html'
+    }),
+    new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery"
+    }),
+    // new GoogleFontsPlugin({
+    //     fonts: [
+    //         { family: "Roboto", variants: [ 
+    //             "300", "400", "500",
+    //             "700", "900" 
+    //         ] }
+    //     ]
+    //     /* ...options */
+    // })
+  ],
+  module: {
+    rules: [
+        {
+          test: /\.(scss)$/,
+          use: [
+              {
+                  loader: 'style-loader', // inject CSS to page
+              }, 
+              {
+                  loader: 'css-loader', // translates CSS into CommonJS modules
+              }, 
+              {
+                  loader: 'postcss-loader', // Run post css actions
+                  options: {
+                      plugins: function () { // post css plugins, can be exported to postcss.config.js
+                          return [
+                              require('precss'),
+                              require('autoprefixer')
+                          ];
+                      }
+                  }
+              }, 
+              {
+                  loader: 'sass-loader' // compiles Sass to CSS
+              }
+          ]
+        },
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+        {
+            test: /\.html$/,
+            use: [
+              {
+                loader: 'html-loader',
+                options: {
+                    minimize: false
+                }
+              }
+            
+            ]
+        },
+        {
+          test: /\.(svg|png|jpe?g|gif|mp4)$/i,
+          use: [
+              {
+                loader: "file-loader",
+                options: {
+                  name: "[name].[hash].[ext]",
+                  outputPath: "img",
+                }
+            }
+        ]
+        },
+    ],
+},
+};
