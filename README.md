e8d7e0fe528d03f8c29d8f3074c8f7604cdaaf4f
Ersoy Efe Uruk <e.uruk@emakina.com.tr>
2020-10-28 20:01:31 +0300

    Update on README and gitignore

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # Airbnb-Clone
+
 This repository is a copy of some pages (fully responsive) of Airbnb web page.
 
 I used Webpack, SCSS, Bootstrap, JQuery.
 
-Here are the links to actual pages: 
+To run, consecutively:
+
+npm i
+
+npm run build
+
+npm start
+
+Here are the links to actual pages:
 
 index: https://www.airbnb.co.uk/
 
@@ -12,4 +21,5 @@ homes: https://www.airbnb.co.uk/s/homes
 rooms: https://www.airbnb.co.uk/rooms/8794484?source_impression_id=p3_1573732036_b7SBeKS8ire8yuN8
 
 Notes:
+
 - Airbnb made changes since I started to implement the project, there might be some differences.
--- webpack.config.js
+++ webpack.config.js
@@ -1,97 +1,94 @@
-const path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 // const GoogleFontsPlugin = require("@beyonk/google-fonts-webpack-plugin");
 // const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-var webpack = require('webpack');
+var webpack = require("webpack");
 
 module.exports = {
-  entry: './src/assets/js/index.js',
+  entry: "./src/assets/js/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  mode: 'production',
+  mode: "production",
   devServer: {
-      contentBase: path.join(__dirname, 'dist'),
-      port: 7000,
-      watchContentBase: true,
-      open: true
+    contentBase: path.join(__dirname, "dist"),
+    port: 7000,
+    watchContentBase: true,
+    open: true,
   },
   plugins: [
     // new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'index.html'
+      template: "./src/index.html",
+      filename: "index.html",
     }),
     new HtmlWebpackPlugin({
-        template: './src/homes.html',
-        filename: 'homes.html'
+      template: "./src/homes.html",
+      filename: "homes.html",
     }),
     new HtmlWebpackPlugin({
-        template: './src/rooms.html',
-        filename: 'rooms.html'
+      template: "./src/rooms.html",
+      filename: "rooms.html",
     }),
     new webpack.ProvidePlugin({
-        $: "jquery",
-        jQuery: "jquery",
-        "window.jQuery": "jquery"
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
     }),
   ],
   module: {
     rules: [
-        {
-          test: /\.(scss)$/,
-          use: [
-              {
-                  loader: 'style-loader', // inject CSS to page
-              }, 
-              {
-                  loader: 'css-loader', // translates CSS into CommonJS modules
-              }, 
-              {
-                  loader: 'postcss-loader', // Run post css actions
-                  options: {
-                      plugins: function () { // post css plugins, can be exported to postcss.config.js
-                          return [
-                              require('precss'),
-                              require('autoprefixer')
-                          ];
-                      }
-                  }
-              }, 
-              {
-                  loader: 'sass-loader' // compiles Sass to CSS
-              }
-          ]
-        },
-        {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-        },
-        {
-            test: /\.html$/,
-            use: [
-              {
-                loader: 'html-loader',
-                options: {
-                    minimize: false
-                }
-              }
-            
-            ]
-        },
-        {
-          test: /\.(svg|png|jpe?g|gif|mp4)$/i,
-          use: [
-              {
-                loader: "file-loader",
-                options: {
-                  name: "[name].[hash].[ext]",
-                  outputPath: "img",
-                }
-            }
-        ]
-        },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: "style-loader", // inject CSS to page
+          },
+          {
+            loader: "css-loader", // translates CSS into CommonJS modules
+          },
+          {
+            loader: "postcss-loader", // Run post css actions
+            options: {
+              plugins: function () {
+                // post css plugins, can be exported to postcss.config.js
+                return [require("precss"), require("autoprefixer")];
+              },
+            },
+          },
+          {
+            loader: "sass-loader", // compiles Sass to CSS
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              minimize: false,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(svg|png|jpe?g|gif|mp4)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[hash].[ext]",
+              outputPath: "img",
+            },
+          },
+        ],
+      },
     ],
-},
+  },
 };
